b0ebe4c51ca219f932f4a549373edaacb0d55240
vicky <vigmurug@gmail.com>
2022-12-25 22:52:36 +0530

    interchanged logo and nav links position

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,23 @@
+import {Link} from 'react-scroll';
 import Logo from '../assets/logo.png';
 import Drawer from './Drawer';
-
 
 function NavBar() {
     return (
       
-      <nav className="navbar grid grid-cols-4" style={{ height: '15vh'}}>
-          <div className="navbar-logo">
-              <a href="/home"><img src={Logo} alt='seas-logo' className='h-16 m-3' /></a>
-          </div>
-          <ul className="hidden sm:grid grid grid-cols-4 col-start-3 col-span-2 justify-items-center p-5">
-            <li><a href="/home">Home</a></li>
-            <li><a href="/services">Services</a></li>
-            <li><a href="/aboutus">About</a></li>
-            <li><a href="/contactus">Contact</a></li>
+      <nav className="navbar grid grid-cols-4">
+          <ul className="hidden sm:grid grid grid-cols-4 col-start-1 col-span-2 justify-items-center p-5 text-lg text-white">
+            <li className="mt-2 cursor-pointer hover:text-green-900"><Link to="home" spy={true} smooth={true}>Home</Link></li>
+            <li className="mt-2 cursor-pointer hover:text-green-900"><Link to="aboutus" spy={true} smooth={true}>About</Link></li>
+            <li className="mt-2"><Link to="services">Services</Link></li>
+            <li className="mt-2 cursor-pointer hover:text-green-900"><Link to="contactus" spy={true} smooth={true}>Contact</Link></li>
           </ul>
-          <Drawer />
+          <div>
+            <Drawer />
+          </div>
+          <div className="navbar-logo grid col-start-4 justify-items-end mr-6">
+              <Link to="home" spy={true} smooth={true}><img src={Logo} alt='seas-logo' className='h-24 cursor-pointer' /></Link>
+          </div>
       </nav>
 
     );
