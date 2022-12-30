cb85adfd88be55bd4c01a1a747fb7b9a08a5f2c8
vicky <vigmurug@gmail.com>
2022-12-30 23:51:08 +0530

    commit

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,7 +2,7 @@ import BgImage from '../assets/home_bg.png';
 
 function HomePage() {
     return (
-      <div id="home" className="bg-yellow-300">
+      <div id="home">
         <img src={BgImage} alt="home" className=" w-[100%]" />
       </div>
     );
