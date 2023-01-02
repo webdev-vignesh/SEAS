f22fa8ca8198a52a472858467bf656bbc78cb0e1
vicky <vigmurug@gmail.com>
2023-01-02 22:17:43 +0530

    created scroll to top element

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,13 +1,15 @@
 import AboutUs from "./AboutUs";
 import ContactUs from "./ContactUs";
 import HomePage from "./HomePage";
+import ScrollToTop from "react-scroll-to-top";
 
 function MainPage() {
     return (
       <div>
-        <div id="home"><HomePage /></div>
-        <div id="about"><AboutUs /></div>
-        <div id="contact"><ContactUs /></div>
+        <HomePage />
+        <AboutUs />
+        <ContactUs />
+        <ScrollToTop smooth color="#3ab54a" width="" />
       </div>
     );
   }
