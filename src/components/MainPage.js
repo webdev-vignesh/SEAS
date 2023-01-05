5443a5726f27bea343570e3a60d71972725bb7bd
vicky <vigmurug@gmail.com>
2023-01-05 23:38:11 +0530

    components updated

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,18 +1,23 @@
 import AboutUs from "./AboutUs";
 import ContactUs from "./ContactUs";
 import HomePage from "./HomePage";
+import NavBar from "./NavBar";
 import ScrollToTop from "react-scroll-to-top";
+import Footer from "./Footer";
+
 
 function MainPage() {
-    return (
-      <div>
-        <HomePage />
-        <AboutUs />
-        <ContactUs />
-        <ScrollToTop smooth color="#3ab54a" width="" />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <NavBar />
+      <HomePage />
+      <AboutUs />
+      <ContactUs />
+      <Footer />
+      <ScrollToTop smooth color="#3ab54a" width="" />
+    </div>
+  );
+}
   
-  export default MainPage;
+export default MainPage;
   
