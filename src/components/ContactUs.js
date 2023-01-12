0bae1a8e2d913671dc71400ed1007fb7dab318dd
vicky <vigmurug@gmail.com>
2023-01-12 23:37:39 +0530

    updated content

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -4,6 +4,9 @@ function ContactUs() {
     return (
       <div id="contactus">
         <img src={contact_img} alt="contact page" className="h-[100vh] w-full"/>
+        <h1>Get In Touch</h1>
+        <p>Want to find out how SEAS can solve problems specific to your business? Let's talk.</p>
+        
       </div>
     );
   }
