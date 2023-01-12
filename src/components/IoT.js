ac5f0089af367393038199ccec2b99a30554d572
vicky <vigmurug@gmail.com>
2023-01-12 23:35:53 +0530

    updated content

--- src/components/IoT.js
+++ src/components/IoT.js
@@ -1,13 +1,45 @@
-import Team from '../assets/aboutus_img.jpg';
+import iot from '../assets/iot.jpg';
 import Footer from './Footer';
 import NavBar from './NavBar';
 
 function IoT() {
     return (
-      <div id="iot">
-        <NavBar />
-        <img src={Team} alt="aboutus" className="w-full h-[100vh]" />   
-        <Footer />
+      <div id="iot" className="flex flex-col h-screen bg-gradient-to-t from-[#0052D4] h-full md:h-[100vh] ">
+        <NavBar className="flex"/>
+        <br />
+        <div className="text-center flex-1 mx-2">
+          <h1 className="text-2xl text-center font-bold ">Internet of Things</h1>
+          <p>
+          Our company specializes in providing innovative IoT solutions and services to help businesses and organizations optimize their operations and gain valuable insights from connected devices and data.
+          We work closely with our clients to understand their unique needs and challenges, and design custom solutions that deliver measurable results.
+          </p>
+          <br />
+          <div className="text-left grid grid-cols-1 md:grid-cols-3 ">
+            <ul className="ml-8 mb-4">
+              <h6 className="font-semibold ">Our IoT solutions include:</h6>
+              <li>▪️ Smart device development and integration</li>
+              <li>▪️ Cloud-based data management and analytics</li>
+              <li>▪️ Remote monitoring and control</li>
+              <li>▪️ Automation and optimization of industrial processes</li>
+              <li>▪️ Predictive maintenance and asset tracking</li>
+            </ul>            
+            <ul className="ml-8">
+              <h6 className="font-semibold">Our ToT services include:</h6>
+              <li>▪️ IoT strategy and consulting</li>
+              <li>▪️ System design and implementation</li>
+              <li>▪️ Device and data security</li>
+              <li>▪️ Ongoing support and maintenance</li>
+            </ul>
+            <div className=" m-4 md:mt-0">
+              <img src={iot} alt="internet of things"  />
+            </div>
+          </div>
+          <p>
+          Whether you're looking to improve efficiency in your manufacturing facility, optimize your supply chain, or gain insights from your product usage data, we can help.
+          Our team of experts has extensive experience in the IoT field, and we stay up-to-date with the latest technologies and industry trends to ensure that we're providing our clients with the best possible solutions.
+        </p>
+        </div>
+        <Footer className="flex"/>
       </div>
     );
   }
