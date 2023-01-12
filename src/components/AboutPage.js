2d3367f42fbe7e324b2c4205c283e54876656230
vicky <vigmurug@gmail.com>
2023-01-12 23:37:24 +0530

    created a seperate page

---
+++ src/components/AboutPage.js
@@ -0,0 +1,35 @@
+import about_img from '../assets/aboutus_img.jpg';
+import Footer from './Footer';
+import NavBar from './NavBar';
+
+function AboutPage() {
+    return (
+      <div id="aboutPage">
+        <NavBar />
+        <div className="m-2">
+          <p className="font-bold text-4xl text-center">About SEAS</p>
+          <p className="font-bold text-left mt-6 md:mt-2 text-2xl">⇗ OUR MISSION</p>
+          <p className="m-1">
+          To help businesses and organizations in increasing efficiency, reducing costs, and improving overall operations by utilising cutting-edge technologies. 
+          We specialize in providing automation solutions for manufacturing and industrial processes, IoT solutions for asset tracking and monitoring, and energy management solutions for reducing energy consumption and costs.
+          </p>
+          <p className="font-bold text-center text-2xl mt-6 md:mt-2">⇌ WHO WE ARE</p>
+          <p className="m-1">
+            We are a team of commited experts who are enthusiastic about technology and applying it to solve problems in the real world.
+            We have been in business for 4 years and have a reputation for providing our clients with cutting-edge, high-quality solutions.
+            Our team of professionals has extensive knowledge in the areas of automation, Internet of Things, and energy management. 
+          </p>
+          <p className="font-bold text-right text-2xl mt-6 md:mt-2">⇯ WHAT WE DO</p>
+          <p>
+            We work closely with our clients to understand their individual requirements and design our solutions to match those requirements. 
+            We understand that every business is unique, and we take great pleasure in providing exceptional customer service and building strong relationships with our clients.
+          </p>
+          <br/>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+  
+  export default AboutPage;
+  
