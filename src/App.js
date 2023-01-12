b7146637cab14deeefc1a9c2d1e1fe2e3bd0c0a5
vicky <vigmurug@gmail.com>
2023-01-12 23:35:39 +0530

    modified react routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import MainPage from "./components/MainPage";
+import HomePage from "./components/HomePage";
 import { Routes, Route } from "react-router-dom";
 import IoT from "./components/IoT";
 import Energy from "./components/Energy";
 import Automation from "./components/Automation";
+import AboutPage from "./components/AboutPage";
+import ContactPage from "./components/ContactPage";
 
 function App() {
   return (
     <div className="App bg-[#3AB54A]">
       <Routes>
-        <Route path="/" element={<MainPage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/energy" element={<Energy />} />
         <Route path="/automation" element={<Automation />} />
         <Route path="/iot" element={<IoT />} />
-        <Route path="*" element={<MainPage />} />
+        <Route path="/aboutus" element={<AboutPage />} />
+        <Route path="/contactus" element={<ContactPage />} />
+        <Route path="*" element={<HomePage />} />
       </Routes>
     </div>
   );
