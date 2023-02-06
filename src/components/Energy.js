c23583516b4285b7cbe01e16742ce1fb3e7c2efa
vicky <vigmurug@gmail.com>
2023-02-06 15:41:48 +0530

    energy management code update

--- src/components/Energy.js
+++ src/components/Energy.js
@@ -1,6 +1,8 @@
 import Footer from './Footer';
 import NavBar from './NavBar';
 import ScrollToTop from "react-scroll-to-top";
+import apfcPanel from '../assets/apfc-panel.png';
+import solarPanel from '../assets/solar-install.jpg';
 import { Helmet } from 'react-helmet';
 
 function Energy() {
@@ -9,15 +11,27 @@ function Energy() {
       <Helmet><title>SEAS Tech | Energy Management</title></Helmet>
       <NavBar className="flex" />
       <div className="flex-1 text-white bg-gradient-to-t from-[#182848] to-[#4b6cb7] " >
+        <br/>
         <h1 className="text-2xl text-center font-bold">Energy Management</h1>
         <br />
-        <br />
-        <p className="text-center ml-2 mr-2">We offer energy management services to help you optimize your energy use and reduce your carbon footprint. Our team can conduct energy audits, provide recommendations for energy-efficient upgrades, and help you implement strategies to reduce your energy consumption.</p> <br/>
-        <p className="ml-2">▪️ If you are looking to switch to renewable energy sources, our team can help you design and install a custom solar panel system for your industrial or commercial property. We will work with you to determine the best location for the panels, as well as the most efficient and cost-effective system for your needs.</p>
-        <p className="ml-2">▪️ Proper maintenance is key to ensuring that your solar panel system is running at its best. Our team offers a range of services, including cleaning and inspection, to keep your system running smoothly. We also offer repairs and troubleshooting services if any issues arise.</p>
+        <p className="text-center ml-2 mr-2">We offer energy management services to help you optimize your energy use and reduce your carbon footprint.</p> 
+        <p className="text-center ml-2 mr-2"> Our team can conduct energy audits, provide recommendations for energy-efficient upgrades, </p> 
+        <p className="text-center ml-2 mr-2">and help you implement strategies to reduce your energy consumption.</p>
         <br/>
-        <br/>
-        <p className="text-center">With years of experience in the industry, we have the knowledge and skills to handle any project, big or small. <br/>
+        <div className="flex-wrap md:flex md:justify-around ">
+          <div className="p-4 md:p-0 md:ml-2 lg:ml-6">
+            <img src={apfcPanel} alt="apfc panel" className="md:h-64 md:w-52 lg:w-64"/>
+          </div>
+          <div className="p-4 md:p-0 ">
+            <h2>Our Energy Management services includes:</h2>
+            <p>▪️ Solar panel installation</p>
+            <p>▪️ APFC panel installation</p>
+          </div>
+          <div className="p-4 md:p-0 md:mr-2 lg:mr-6">
+            <img src={solarPanel} alt="solar panel" className="md:h-64 md:w-52 lg:w-64"/>
+          </div>  
+        </div>
+        <p className="text-center mt-1">With years of experience in the industry, we have the knowledge and skills to handle any project, big or small. <br/>
         Our team of highly trained and experienced professionals is dedicated to providing top-quality service to our clients.</p>  
         <br/>
       </div> 
